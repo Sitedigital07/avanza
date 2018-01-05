a9ebcbb210e351a47631549c35680a6f07a55799
Dario Enrique Martinez Zabala <info@sitedigital.com.co>
2018-01-04 20:38:34 -0500

    Ajustes avanza
    Ajustes avanza

--- views/fichaje/avanza.blade.php
+++ views/fichaje/avanza.blade.php
@@ -31,10 +31,18 @@
   @include('Template.menu')
 
 
-  <div class="container">
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
 
-  <ul class="nav nav-tabs" role="tablist" style="margin-top:40px">
+  <div class="container">
+    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+
+       <?php $status=Session::get('status');?>
+    @if($status=='ok_create')
+      <div class="alert alert-success">
+       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+       <strong>Su mensaje ha sido enviado con éxtio</strong> </div>
+    @endif
+
+  <ul class="nav nav-tabs" role="tablist">
     <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Descrtipción</a></li>
     <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Datos contacto</a></li>
     <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Contactar Empresa</a></li>
@@ -47,7 +55,7 @@
   <!-- Tab panes -->
   <div class="tab-content">
     <div role="tabpanel" class="tab-pane active" id="home">
-     <div class="col-xs-7 col-sm-7 col-md-7 col-lg-8">    
+     <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">    
       @foreach($contenido as $contenido)
       <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 detalle">
       <div class="thumbnail">
@@ -70,7 +78,7 @@
     <div role="tabpanel" class="tab-pane" id="profile">
      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 detalle">
       <div class="thumbnail">
-       <iframe src="{{$contenido->ubicacion}}" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+       <iframe src="{{$contenido->ubicacion}}" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
       <div class="caption">
       <h2>{{$contenido->title}}</h2>
        <ul class="list-group">
@@ -88,44 +96,43 @@
 
     <div role="tabpanel" class="tab-pane" id="messages">
     
-    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8" style="top:-25px">
 
-        {{ Form::open(array('method' => 'POST','class' => 'form-horizontal','id' => 'defaultForm', 'url' => array('mensajes/mensajeficha'))) }}
-    <div class="form-group col-md-12">
-       {{Form::label('nombre', 'Nombre' )}}
-       {{Form::text('nombre', '', array('class' => 'form-control','placeholder'=>'Ingrese su Contraseña'))}}
-    </div>
+     <h3>Ponte en contacto con nosostros</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lectus lectus, tempus in sollicitudin in, gravida dignissim nulla. Aliquam nisi nulla, laoreet sit amet neque id, gravida rhoncus augue.</p>
+<br>
+<form method="POST" action="/mensajes/mensajeficha">
+  <div class="form-group">
+    <label for="email">Nombre:</label>
+    <input type="text" class="form-control" id="email" name="nombre">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Empresa:</label>
+    <input type="text" class="form-control" id="pwd" name="sujeto">
+  </div>
+   <div class="form-group">
+    <label for="pwd">Número contacto:</label>
+    <input type="text" class="form-control" id="pwd" name="datos">
+  </div>
+   <div class="form-group">
+    <label for="pwd">Correo electrónico:</label>
+    <input type="email" class="form-control" id="pwd" name="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Mensaje:</label>
+    <textarea class="form-control" rows="8" id="comment" name="mensaje"></textarea>
+  </div>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+  
+  <input type="hidden" class="form-control" id="pwd" value="{{Request::segment(2)}}" name="empresa">
+   <input type="hidden" class="form-control" id="pwd" value="empresa/{{Request::segment(2)}}" name="redireccion">
+  <input type="hidden" class="form-control" id="pwd" value="{{$contenido->usuario_id}}" name="interes">
+  <input type="hidden" class="form-control" id="pwd" value="{{$contenido->email}}" name="ema">
+  <button type="submit" class="btn btn-primary col-lg-12">Enviar</button>
+</form>
 
-    <div class="form-group col-md-12">
-       {{Form::label('sujeto', 'Empresa' )}}
-       {{Form::text('sujeto', '', array('class' => 'form-control','placeholder'=>'Ingrese su Contraseña','placeholder'=>'Ingrese el Nombre de su Empresa'))}}<br>
-    </div>
 
-    <div class="form-group col-md-12">
-       {{Form::label('datos', 'Número de contacto' )}}
-       {{Form::text('datos', '', array('class' => 'form-control','placeholder'=>'Ingrese teléfono o celular de contacto'))}}<br>
-    </div>
 
-    <div class="form-group col-md-12">
-       {{Form::label('email', 'Correo electrónico' )}}
-       {{Form::text('email', '', array('class' => 'form-control','placeholder'=>'Ingrese su Contraseña','placeholder'=>'Ingrese su email'))}}<br>
-    </div>
-      
-       {{Form::hidden('empresa', Request::segment(2), array('class' => 'form-control','placeholder'=>'Ingrese su Contraseña','placeholder'=>'Ingrese su email'))}}
-       {{Form::hidden('interes', $contenido->usuario_id, array('class' => 'form-control'))}}
-     
-    <div class="form-group col-md-12">
-       {{Form::label('mensaje', 'Mensaje' )}}
-       {{Form::textarea('mensaje', '', array('class' => 'form-control','placeholder'=>'Ingrese su Contraseña','placeholder'=>'Ingrese su mensaje'))}}<br>  
-   </div>
-
-       {{Form::hidden('redireccion', Request::path(), array('class' => 'form-control'))}}
-
-           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-             <button type="submit" class="btn btn-primary pull-right">Enviar</button>  
-         </div>
-      {{ Form::close() }}
       </div>
 
     </div>
